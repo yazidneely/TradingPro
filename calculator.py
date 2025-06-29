37a2b4ac7c3a077131c8c9fe6c11ba649cfda255
yazidneely <125466904+yazidneely@users.noreply.github.com>
2025-06-29 18:00:22 +0000

    Updated yfinance version to allow more queries. Improved error handling

--- calculator.py
+++ calculator.py
@@ -246,31 +246,39 @@ def main_gui():
                 loading_window.close()
                 result = result_holder['result']
 
-                avg_volume_bool    = result['avg_volume']
-                iv30_rv30_bool     = result['iv30_rv30']
-                ts_slope_bool      = result['ts_slope_0_45']
-                expected_move      = result['expected_move']
+                try:
+                    avg_volume_bool    = result['avg_volume']
+                    iv30_rv30_bool     = result['iv30_rv30']
+                    ts_slope_bool      = result['ts_slope_0_45']
+                    expected_move      = result['expected_move']
+
                 
-                if avg_volume_bool and iv30_rv30_bool and ts_slope_bool:
-                    title = "Recommended"
-                    title_color = "#006600"
-                elif ts_slope_bool and ((avg_volume_bool and not iv30_rv30_bool) or (iv30_rv30_bool and not avg_volume_bool)):
-                    title = "Consider"
-                    title_color = "#ff9900"
-                else:
-                    title = "Avoid"
-                    title_color = "#800000"
-                
-                result_layout = [
-                    [sg.Text(title, text_color=title_color, font=("Helvetica", 16))],
-                    [sg.Text(f"avg_volume: {'PASS' if avg_volume_bool else 'FAIL'}", text_color="#006600" if avg_volume_bool else "#800000")],
-                    [sg.Text(f"iv30_rv30: {'PASS' if iv30_rv30_bool else 'FAIL'}", text_color="#006600" if iv30_rv30_bool else "#800000")],
-                    [sg.Text(f"ts_slope_0_45: {'PASS' if ts_slope_bool else 'FAIL'}", text_color="#006600" if ts_slope_bool else "#800000")],
-                    [sg.Text(f"Expected Move: {expected_move}", text_color="blue")],
-                    [sg.Button("OK")]
-                ]
-                
-                result_window = sg.Window("Recommendation", result_layout, modal=True, finalize=True, size=(275, 200))
+                    if avg_volume_bool and iv30_rv30_bool and ts_slope_bool:
+                        title = "Recommended"
+                        title_color = "#006600"
+                    elif ts_slope_bool and ((avg_volume_bool and not iv30_rv30_bool) or (iv30_rv30_bool and not avg_volume_bool)):
+                        title = "Consider"
+                        title_color = "#ff9900"
+                    else:
+                        title = "Avoid"
+                        title_color = "#800000"
+                    
+                    result_layout = [
+                        [sg.Text(title, text_color=title_color, font=("Helvetica", 16))],
+                        [sg.Text(f"avg_volume: {'PASS' if avg_volume_bool else 'FAIL'}", text_color="#006600" if avg_volume_bool else "#800000")],
+                        [sg.Text(f"iv30_rv30: {'PASS' if iv30_rv30_bool else 'FAIL'}", text_color="#006600" if iv30_rv30_bool else "#800000")],
+                        [sg.Text(f"ts_slope_0_45: {'PASS' if ts_slope_bool else 'FAIL'}", text_color="#006600" if ts_slope_bool else "#800000")],
+                        [sg.Text(f"Expected Move: {expected_move}", text_color="blue")],
+                        [sg.Button("OK")]
+                    ]
+                except Exception as e:
+                    print(result)
+                    result_layout = [
+                        [sg.Text(result,text_color="#800000")],
+                        [sg.Button("OK")]
+                    ]
+
+                result_window = sg.Window("Recommendation", result_layout, modal=True, finalize=True, size=(475, 200))
                 while True:
                     event_result, _ = result_window.read(timeout=100)
                     if event_result in (sg.WINDOW_CLOSED, "OK"):
